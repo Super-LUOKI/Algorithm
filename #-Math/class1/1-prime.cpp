d9528dbf4b780551d56490c469074044cbc6c712
Zhu-Yinghua <zhuyinghua@heygen.com>
2023-09-08 20:03:06 +0800

    feat: prime number determination

--- #-Math/class1/1-prime.cpp
+++ #-Math/class1/1-prime.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
+
 using namespace std;
 
-bool is_prime(int n){
-    return true
+// 尝除法：判断是否是质数
+bool is_prime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= n / i; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
 }
 
-int main(){
 
+int main() {
     return 0;
 }
